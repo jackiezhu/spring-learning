927d8d25bed43b230a6f266792b4a9367b762dda
JackieZhu <zhuzewei0927@gmail.com>
2015-05-27 23:45:32 +0800

    hello

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by JackieZhu on 15/5/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
